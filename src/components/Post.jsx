c6b373f7276a24c27196306aeb15484aa9e2ab98
Iago Teles <telesiago@gmail.com>
2025-03-18 18:28:54 -0300

    create Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,26 @@
-export function Post({author, content}){
+import styles from './Post.module.css'
+
+export function Post(){
     return(
-        <div>
-            <strong>{author}</strong>
-            <p>{content}</p>
-        </div>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/telesiago.png"/>
+                    <div className={styles.authorInfo}>
+                        <strong>Iago Teles</strong>
+                        <span>Developer</span>
+                    </div>
+                </div>
+
+                <time title='18 de Março às 17:22' datetime="2025-03-18 17:22:00">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p><a href="#">👉 jane.design/doctorcare</a></p>
+                <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+        </article>
     )
 }
