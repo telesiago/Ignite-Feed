63174cf47c449e17986db08bccfb5feb0dddfb28
Iago Teles <telesiago@gmail.com>
2025-03-18 20:55:09 -0300

    create avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import {PencilLine} from '@phosphor-icons/react'
 
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 
 
 export function Sidebar(){
@@ -9,11 +10,8 @@ export function Sidebar(){
             <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
 
             <div className={styles.profile}>
-                <img 
-                    className={styles.avatar} 
-                    src="https://github.com/telesiago.png"
-                />
-
+                <Avatar src='https://github.com/telesiago.png'/>
+                
                 <strong>Iago Teles</strong>
                 <span>Developer</span>
             </div>
