9e88a082440375c81abadb934ec3be2d7f8f6591
Iago Teles <telesiago@gmail.com>
2025-03-18 18:11:56 -0300

    config Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import { Header } from "./components/Header"
 
+import styles from './App.module.css'
+
+import './global.css'
+import { Post } from "./components/Post"
+import { Sidebar } from "./components/Sidebar"
 function App() {
 
   return (
     <>
       <Header/>
-      <h1>Deus grego</h1>
+      
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post 
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ex beatae quis totam quo quia quas laudantium omnis nobis nihil eius rerum architecto tempora deserunt recusandae, odio sint rem pariatur."
+          />
+          <Post 
+            author="Iago Teles"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ex beatae quis totam quo quia quas laudantium omnis nobis nihil eius rerum architecto tempora deserunt recusandae, odio sint rem pariatur."
+          />
+        </main>
+      </div>
     </>
   )
 }
