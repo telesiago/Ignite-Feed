735297a685cf5a5a02c3fc03733613b3c86b3634
Iago Teles <telesiago@gmail.com>
2025-03-19 08:59:20 -0300

    iterating

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,40 @@ import { Post } from "./components/Post"
 import styles from './App.module.css'
 import './global.css'
 
+// author: { avatar_url: "", name: "", role: ""}
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://githuba.com/telesiago.png",
+      name: "Iago Teles",
+      role: "Developer"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '👉 jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2025-03-19 07:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://githuba.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '👉 jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2025-03-20 07:00:00'),
+  }
+]
 function App() {
 
   return (
@@ -14,14 +48,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post 
-            author="Diego Fernandes"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ex beatae quis totam quo quia quas laudantium omnis nobis nihil eius rerum architecto tempora deserunt recusandae, odio sint rem pariatur."
-          />
-          <Post 
-            author="Iago Teles"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ex beatae quis totam quo quia quas laudantium omnis nobis nihil eius rerum architecto tempora deserunt recusandae, odio sint rem pariatur."
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
